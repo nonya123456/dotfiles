85a37feb574c7beecdf6ca7adbbc401412265ae1
desertmouse <nonya579@gmail.com>
2026-05-29 22:33:48 +0700

    enable hidden file search in telescope, exclude .git

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -389,13 +389,15 @@ require('lazy').setup({
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          -- mappings = {
+          --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          -- },
+        },
+        pickers = {
+          find_files = { hidden = true, file_ignore_patterns = { '^%.git/' } },
+          live_grep = { additional_args = { '--hidden', '--glob=!.git' } },
+        },
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
         },
